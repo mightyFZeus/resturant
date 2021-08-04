53eee2f20f06d59c6de89c99a1a012cf7f362692
mightyFZeus <bolarinolabisi36@gmail.com>
2021-08-04 19:27:05 +0100

    reset

--- src/components/Explore/Explore.js
+++ src/components/Explore/Explore.js
@@ -1,26 +1,13 @@
 import React from 'react'
 import Rice from './Breastfast/Rice'
-import Lunch from './Lunch/Lunch'
-import {Link, Route, Switch, BrowserRouter as Router} from 'react-router-dom'
+
+
 
 const Explore = () => {
     return (
-        <Router>
         <div>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to ='/rice'>Breaskfast</Link>
-                    </li>
-                    <li ><Link to='/lunch'>Lunch</Link></li>
-                </ul>
-            </nav>
-            <Switch>
-                <Route exact path='/rice' component={Rice} />
-                <Route exact path='/lunch' component={Lunch} />
-            </Switch>
+            <Rice />
         </div>
-        </Router>
     )
 }
 
