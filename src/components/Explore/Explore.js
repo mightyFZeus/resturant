7ae46f8b7eb1d0b5335aa85cad8b832db8833f6a
mightyFZeus <bolarinolabisi36@gmail.com>
2021-08-04 19:17:28 +0100

    component slide test

--- src/components/Explore/Explore.js
+++ src/components/Explore/Explore.js
@@ -1,11 +1,26 @@
 import React from 'react'
-import Rice from './Rice/Rice'
+import Rice from './Breastfast/Rice'
+import Lunch from './Lunch/Lunch'
+import {Link, Route, Switch, BrowserRouter as Router} from 'react-router-dom'
 
 const Explore = () => {
     return (
+        <Router>
         <div>
-            <Rice />
+            <nav>
+                <ul>
+                    <li>
+                        <Link to ='/rice'>Breaskfast</Link>
+                    </li>
+                    <li ><Link to='/lunch'>Lunch</Link></li>
+                </ul>
+            </nav>
+            <Switch>
+                <Route exact path='/rice' component={Rice} />
+                <Route exact path='/lunch' component={Lunch} />
+            </Switch>
         </div>
+        </Router>
     )
 }
 
